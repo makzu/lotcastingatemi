10bc5c4fbae7a6165de5a8f3f639db98c12492a5
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2017-03-24 22:30:46 -0700

    Make sure seeds actually work in an empty DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
 puts "Reloading example player"
-player1 = Player.find_or_create_by(id: 1)
+player1 = Player.find_or_initialize_by(id: 1)
 player1.update(
   name: "Example player",
   email: "solarShard179@IAM.net",
@@ -8,7 +8,7 @@ player1.update(
 )
 
 puts "Reloading example chronicle"
-exChronicle = Chronicle.find_or_create_by(id: 1)
+exChronicle = Chronicle.find_or_initialize_by(id: 1)
 exChronicle.update(
   name: "Example game",
   player: player1
@@ -16,42 +16,42 @@ exChronicle.update(
 
 puts "Reloading characters"
 YAML.load(File.read("db/seeds/characters.yaml")).each do |c|
-  character = Character.find_or_create_by(id: c["id"])
+  character = Character.find_or_initialize_by(id: c["id"])
   character.update(c)
   puts "Updated " + character.name
 end
 
 puts "Reloading merits"
 YAML.load(File.read("db/seeds/merits.yaml")).each do |m|
-  merit = Merit.find_or_create_by(id: m["id"])
+  merit = Merit.find_or_initialize_by(id: m["id"])
   merit.update(m)
   puts "Updated " + merit.name + " for " + merit.character.name
 end
 
 puts "Reloading weapons"
 YAML.load(File.read("db/seeds/weapons.yaml")).each do |w|
-  weapon = Weapon.find_or_create_by(id: w["id"])
+  weapon = Weapon.find_or_initialize_by(id: w["id"])
   weapon.update(w)
   puts "Updated " + weapon.name + " for " + weapon.character.name
 end
 
 puts "Reloading QCs"
 YAML.load(File.read("db/seeds/qcs.yaml")).each do |q|
-  qc = Qc.find_or_create_by(id: q["id"])
+  qc = Qc.find_or_initialize_by(id: q["id"])
   qc.update(q)
   puts "Updated " + qc.name
 end
 
 puts "Reloading QC merits"
 YAML.load(File.read("db/seeds/qc_merits.yaml")).each do |m|
-  merit = QcMerit.find_or_create_by(id: m["id"])
+  merit = QcMerit.find_or_initialize_by(id: m["id"])
   merit.update(m)
   puts "Updated " + merit.name + " for " + merit.qc.name
 end
 
 puts "Reloading QC attacks"
 YAML.load(File.read("db/seeds/qc_attacks.yaml")).each do |a|
-  attack = QcAttack.find_or_create_by(id: a["id"])
+  attack = QcAttack.find_or_initialize_by(id: a["id"])
   attack.update(a)
   puts "Updated " + attack.name + " for " + attack.qc.name
 end

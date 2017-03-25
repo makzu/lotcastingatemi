757f7afc514252828b82a010a56ff4948ae9b453
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2017-03-24 20:15:54 -0700

    oops

--- app/models/qc.rb
+++ app/models/qc.rb
@@ -16,19 +16,15 @@ class Qc < ApplicationRecord
   # Essence above 5 is explicitly mentioned in the book
   validates_numericality_of :essence, greater_than_or_equal_to: 1, less_than_or_equal_to: 10
 
-  validates_numericality_of
-      :motes_personal_current,
-      :motes_personal_total,
-      :motes_peripheral_current,
-      :motes_peripheral_total,
+  validates_numericality_of :motes_personal_current, :motes_personal_total,
+      :motes_peripheral_current, :motes_peripheral_total,
 
       :grapple, :grapple_control,
       :hardness,
       :initiative, :onslaught,
     greater_than_or_equal_to: 0
 
-  validates_numericality_of
-      :resolve, :guile, :appearance,
+  validates_numericality_of :resolve, :guile, :appearance,
       :evasion, :parry, :soak,
       :movement,
 

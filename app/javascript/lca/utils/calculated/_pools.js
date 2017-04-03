010e8f075a343a894e6fbe0e3721883313d9885c
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2017-04-03 13:05:27 -0700

    Split weapon calculations into their own file

--- app/javascript/lca/utils/calculated/_pools.js
+++ app/javascript/lca/utils/calculated/_pools.js
@@ -1,48 +1,55 @@
-
 export function joinBattlePool(character) {
   // TODO handle merits that affect jb pool
   // TODO specialties
+  // TODO penalties
   return character.attr_wits + character.abil_awareness
 }
 
 export function rushPool(character) {
   // TODO handle merits that affect pool
   // TODO specialties
+  // TODO penalties
   return character.attr_dexterity + character.abil_athletics
 }
 
 export function disengagePool(character) {
   // TODO handle merits that affect pool
   // TODO specialties
+  // TODO penalties
   return character.attr_dexterity + character.abil_dodge
 }
 
 export function riseFromPronePool(character) {
   // TODO handle merits that affect pool
   // TODO specialties
+  // TODO penalties
   return character.attr_dexterity + character.abil_dodge
 }
 
 export function takeCoverPool(character) {
   // TODO handle merits that affect pool
   // TODO specialties
+  // TODO penalties
   return character.attr_dexterity + character.abil_dodge
 }
 
 export function withdrawPool(character) {
   // TODO handle merits that affect pool
   // TODO specialties
+  // TODO penalties
   return character.attr_dexterity + character.abil_dodge
 }
 
 export function readIntentionsPool(character) {
   // TODO handle merits that affect pool
   // TODO specialties
+  // TODO penalties
   return character.attr_perception + character.abil_socialize
 }
 
 export function shapeSorceryPool(character) {
   // TODO handle merits that affect pool
   // TODO specialties
+  // TODO penalties
   return character.attr_intelligence + character.abil_occult
 }
--- app/javascript/lca/utils/calculated/index.js
+++ app/javascript/lca/utils/calculated/index.js
@@ -1,25 +1,34 @@
-/* Defense values (Parry is per-weapon and in weapons.js) */
 import { ATTACK_ABILITIES, ABILITIES, ABILITIES_ALL } from '../constants.js'
 export {
   joinBattlePool, rushPool, disengagePool, riseFromPronePool, takeCoverPool,
   withdrawPool, readIntentionsPool, shapeSorceryPool
 } from './_pools.js'
+export {
+  weaponAccuracyBonus, weaponDamageBonus, weaponDamageType, weaponDefenseBonus,
+  weaponDamage, weaponOverwhelming, witheringAttackPool, decisiveAttackPool,
+  weaponParry
+} from './_weapons.js'
 
+/* Defense values (Parry is per-weapon) */
 export function evasionRaw(character) {
   // TODO specialties
+  // TODO penalties
   return Math.ceil((character.attr_dexterity + character.abil_dodge) / 2)
 }
 
 export function guileRaw(character) {
   // TODO specialties
+  // TODO penalties
   return Math.ceil((character.attr_manipulation + character.abil_socialize) / 2)
 }
 
 export function resolveRaw(character) {
   // TODO specialties
+  // TODO penalties
   return Math.ceil((character.attr_wits + character.abil_integrity) / 2)
 }
 
+/* Health */
 export function totalHealthLevels(character) {
   return character.health_level_0s + character.health_level_1s +
     character.health_level_2s + character.health_level_4s +
@@ -70,110 +79,6 @@ export function abilitiesWithRatings(character) {
   })
 
   return abils
-}
-
-
-export function weaponAccuracyBonus(weapon) {
-  // TODO: thrown/archery
-  switch(weapon.weight) {
-  case "light":
-    return weapon.is_artifact ? 5 : 4
-  case "medium":
-    return weapon.is_artifact ? 3 : 2
-  case "heavy":
-    return weapon.is_artifact ? 1 : 0
-  }
-}
-
-export function weaponDamageBonus(weapon) {
-  // TODO: thrown/archery
-  let damage = 0
-
-  switch(weapon.weight) {
-  case "light":
-    damage = weapon.is_artifact ? 10 : 7
-    break
-  case "medium":
-    damage = weapon.is_artifact ? 12 : 9
-    break
-  case "heavy":
-    damage = weapon.is_artifact ? 14 : 11
-    break
-  }
-
-  if (weapon.tags.includes("shield"))
-    damage -= 2
-
-  return damage
-}
-
-export function weaponDamageType(weapon) {
-  if (weapon.tags.includes("bashing"))
-    return "B"
-  else if (weapon.tags.includes("lethal"))
-    return "L"
-  else if (weapon.tags.includes("aggravated"))
-    return "A"
-  else
-    return "B"
-}
-
-export function weaponDefenseBonus(weapon) {
-  switch(weapon.weight) {
-  case "light":
-    return weapon.is_artifact ? 0 : 0
-  case "medium":
-    return weapon.is_artifact ? 1 : 1
-  case "heavy":
-    return weapon.is_artifact ? 0 : -1
-  }
-}
-
-export function weaponDamage(character, weapon) {
-  let damage = weaponDamageBonus(weapon)
-
-  if (weapon.tags.includes("crossbow") || weapon.tags.includes("flame")) {
-    damage += 4
-  } else {
-    damage += character.attr_strength
-  }
-
-  return damage
-}
-
-export function weaponOverwhelming(weapon) {
-  if (! weapon.is_artifact)
-    return 0
-  switch(weapon.weight) {
-  case "light":
-    return 3
-  case "medium":
-    return 4
-  case "heavy":
-    return 5
-  }
-}
-
-export function witheringAttackPool(character, weapon) {
-  // TODO specialties
-  return decisiveAttackPool(character, weapon) + weaponAccuracyBonus(weapon)
-}
-
-export function decisiveAttackPool(character, weapon) {
-  // TODO specialties
-  const ability = attackAbilities(character).find((abil) =>
-    abil.abil == weapon.ability
-  )
-  if (ability == undefined)
-    return character.attr_dexterity
-
-  return character.attr_dexterity + ability.rating
-}
-
-export function weaponParry(character, weapon) {
-  // TODO handle archery and thrown
-  // TODO specialties
-  return Math.ceil(decisiveAttackPool(character, weapon) / 2) + weaponDefenseBonus(weapon)
 }
 
 export function mobilityPenalty(character) {

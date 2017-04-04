eab07d52d7a12f97e1ac4952cc91cf1c3ef7931d
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2017-04-03 17:38:02 -0700

    Clean up some unused files

--- app/javascript/lca/selectors/index.js
+++
@@ -1,6 +0,0 @@
-import { createSelector } from 'reselect'
-
-const character = (state) => state.characters.characters
-
-export const computedValues = createSelector([character], (character) => {
-})

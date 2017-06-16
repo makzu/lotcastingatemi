408e69adee78879895e0fe689df465c81019270f
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2017-06-15 19:14:11 -0700

    Use styled-components for RatingDots

--- app/javascript/lca/components/generic/ratingDots.jsx
+++ app/javascript/lca/components/generic/ratingDots.jsx
@@ -1,22 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+// TODO: Replace with SVG icons or something?
+const RatingDotContainer = styled.div`
+  float: right;
+  clear: right;
+`
+const RatingDot = styled.div`
+  display: inline-block;
+  color: transparent;
+  width: 0.75em;
+  height: 0.75em;
+  border-radius: 0.45em;
+  border: 0.125em solid black;
+  margin-left: 1px;
+  background-color: white;
+`
+const EmptyRatingDot = styled(RatingDot)``
+const FullRatingDot = styled(RatingDot)`
+  background-color: black;
+`
 
 function RatingDots(props) {
   const emptyCount = ( props.fillTo || 5 ) - props.rating
 
   const fullDots = new Array(props.rating).fill('●')
   const fullDivs = fullDots.map((dot, index) =>
-    <div key={ index } className="ratingDot ratingDotFull">{dot}</div>
+    <FullRatingDot key={ index }>{dot}</FullRatingDot>
   )
   let emptyDivs = ''
 
   if (props.dontFill == null && emptyCount >= 0) {
     let emptyDots = new Array(emptyCount).fill('○')
     emptyDivs = emptyDots.map((dot, index) =>
-      <div key={ index } className="ratingDot ratingDotEmpty">{dot}</div>
+      <EmptyRatingDot key={ index }>{dot}</EmptyRatingDot>
     )
   }
-  return <span className="ratingDotContainer">{ fullDivs }{ emptyDivs }</span>
+  return <RatingDotContainer>{ fullDivs }{ emptyDivs }</RatingDotContainer>
 }
 
 RatingDots.propTypes = {

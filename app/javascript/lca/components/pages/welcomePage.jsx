5deb9de889cc9f5a763e08c61d8a9a76e4118280
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2017-07-18 23:46:31 -0700

    Fix up Charm text on welcome page

--- app/javascript/lca/components/pages/welcomePage.jsx
+++ app/javascript/lca/components/pages/welcomePage.jsx
@@ -4,8 +4,10 @@ export default function WelcomePage() {
   return(<div className="welcomePage">
     <h1>Lot-Casting Atemi</h1>
     <p>
-      <strong>Type:</strong> Reflexive,
-      <strong> Cost:</strong> 15m, 1wp
+      <strong>Cost:</strong> 15m, 1wp, <strong>Mins:</strong> Essence 2<br />
+      <strong>Type:</strong> Simple<br />
+      <strong>Keywords:</strong> None<br />
+      <strong>Duration:</strong> One scene
     </p>
     <p>
       The Solar Exalted have achieved such heights of skill that even their
@@ -13,7 +15,7 @@ export default function WelcomePage() {
       instantly becomes aware of her scores and those of the other players. She
       likewise never forgets the strength of her signature moves, or the number
       of dice required to play them. She can roll those dice with but a
-      thought, and no matter how distant she is from the game all participants
+      thought, and no matter how distant she is from the game, all participants
       become aware of her successes.
     </p>
   </div>)

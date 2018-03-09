b6074c90f9b101e2c1b0e56d45a69265ad9517d7
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2018-03-09 00:20:22 -0800

    Add authentication to websocket connections

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,5 +2,21 @@
 
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_player
+
+    def connect
+      self.current_player = find_verified_user
+    end
+
+    protected
+
+    def find_verified_user
+      token = Knock::AuthToken.new token: request.params[:token]
+
+      verified_user = Player.find(token.payload['sub'])
+      return verified_user if verified_user
+
+      reject_unauthorized_connection
+    end
   end
 end

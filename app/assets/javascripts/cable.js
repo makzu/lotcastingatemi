aa5967ea8a1bb1aaeb61240287ec9433202fd1c6
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2018-03-10 10:42:39 -0800

    Revert "Add authentication to websocket connections"
    This reverts commit b6074c90f9b101e2c1b0e56d45a69265ad9517d7.

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -8,6 +8,6 @@
 (function() {
   this.App || (this.App = {});
 
-  App.cable = ActionCable.createConsumer('/cable?token=' + localStorage.jwt);
+  App.cable = ActionCable.createConsumer();
 
 }).call(this);

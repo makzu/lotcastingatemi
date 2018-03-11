25afeccf4b14504ab4fc08e25f361e83389037f5
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2018-03-10 21:07:29 -0800

    Broadcast changes to player display names

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,8 @@
 
 # User account.
 class Player < ApplicationRecord
+  after_save :broadcast_update, if: :saved_change_to_display_name?
+
   has_many :identities, dependent: :destroy
 
   has_many :own_chronicles, class_name: 'Chronicle', foreign_key: 'st_id', inverse_of: :st, dependent: :destroy
@@ -24,6 +26,14 @@ class Player < ApplicationRecord
 
   def entity_type
     'player'
+  end
+
+  def broadcast_update
+    UpdateBroadcastJob.perform_later(
+      (own_chronicles + chronicles).map { |x| x.player_ids + [x.st_id] }.flatten.uniq,
+      self,
+      saved_changes.delete_if { |k| k == 'updated_at' }
+    )
   end
 
   def self.create_from_oauth(auth)

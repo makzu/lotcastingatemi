99db8e00a135cc39b519ed68ef8cee2f2d003172
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2018-03-10 22:25:11 -0800

    Enable creating custom attribute and ability exalts

--- app/javascript/lca/components/characters/characterCreatePopup.jsx
+++ app/javascript/lca/components/characters/characterCreatePopup.jsx
@@ -8,9 +8,12 @@ import Dialog, {
   DialogContent,
   DialogTitle,
 } from 'material-ui/Dialog'
+import Divider from 'material-ui/Divider'
 import { FormControlLabel } from 'material-ui/Form'
-import Radio, { RadioGroup } from 'material-ui/Radio'
+import { MenuItem } from 'material-ui/Menu'
+import Switch from 'material-ui/Switch'
 import TextField from 'material-ui/TextField'
+import Typography from 'material-ui/Typography'
 
 import { createCharacter } from '../../ducks/actions.js'
 
@@ -21,11 +24,12 @@ class CharacterCreatePopup extends React.Component {
 
     this.state = {
       open: false,
-      character: { name: '', type: 'Character' },
+      character: { name: '', caste: '', type: 'SolarCharacter', exalt_type: '', aspect: false },
     }
     this.handleOpen = this.handleOpen.bind(this)
     this.handleClose = this.handleClose.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleAspectChange = this.handleAspectChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
@@ -38,7 +42,23 @@ class CharacterCreatePopup extends React.Component {
   }
 
   handleChange(e) {
-    this.setState({ character: { ...this.state.character, [e.target.name]: e.target.value }})
+    const { name, value } = e.target
+    let exaltType = {}
+
+    if (name == 'type') {
+      if (value == 'Character')
+        exaltType = { exalt_type: 'Mortal' }
+      else if (value == 'SolarCharacter')
+        exaltType = { exalt_type: 'Solar' }
+      else
+        exaltType = { exalt_type: 'Exalt' }
+    }
+
+    this.setState({ character: { ...this.state.character, [name]: value, ...exaltType }})
+  }
+
+  handleAspectChange() {
+    this.setState({ character: { ...this.state.character, aspect: !this.state.character.aspect }})
   }
 
   handleSubmit() {
@@ -47,7 +67,7 @@ class CharacterCreatePopup extends React.Component {
   }
 
   render() {
-    const { handleOpen, handleClose, handleChange, handleSubmit } = this
+    const { handleOpen, handleClose, handleChange, handleAspectChange, handleSubmit } = this
     const { character } = this.state
 
     return <span>
@@ -58,17 +78,60 @@ class CharacterCreatePopup extends React.Component {
       >
         <DialogTitle>Create New Character</DialogTitle>
         <DialogContent>
-          <TextField name="name" value={ character.name }
-            label="Name" margin="normal"
-            onChange={ handleChange }
-          />
-          <RadioGroup name="type"
-            value={ character.type }
-            onChange={ handleChange }
-          >
-            <FormControlLabel value="Character" control={ <Radio /> } label="Mortal" />
-            <FormControlLabel value="SolarCharacter" control={ <Radio /> } label="Solar Exalt" />
-          </RadioGroup>
+          <div>
+            <TextField name="name" value={ character.name }
+              label="Name" margin="normal" fullWidth
+              onChange={ handleChange }
+            />
+          </div>
+
+          <div>
+            <TextField select name="type" value={ character.type }
+              label={ character.type == 'Character' ? 'Character Type' : 'Exalt Type ' }
+              onChange={ handleChange } fullWidth margin="normal"
+            >
+              <MenuItem value="Character">Mortal</MenuItem>
+              <MenuItem value="SolarCharacter">Solar Exalt</MenuItem>
+
+              <Divider />
+
+              <MenuItem value="CustomAbilityCharacter">Custom Ability Exalt</MenuItem>
+              <MenuItem value="CustomAttributeCharacter">Custom Attribute Exalt</MenuItem>
+            </TextField>
+          </div>
+
+          { character.type == 'SolarCharacter' &&
+            <div>
+              <TextField select name="caste" value={ character.caste }
+                label="Caste" margin="dense" fullWidth
+                onChange={ handleChange }
+              >
+                <MenuItem value="dawn">Dawn</MenuItem>
+                <MenuItem value="zenith">Zenith</MenuItem>
+                <MenuItem value="twilight">Twilight</MenuItem>
+                <MenuItem value="night">Night</MenuItem>
+                <MenuItem value="eclipse">Eclipse</MenuItem>
+              </TextField>
+            </div>
+          }
+          { (character.type == 'CustomAttributeCharacter' || character.type == 'CustomAbilityCharacter') &&
+            <Typography>
+              <TextField name="caste" value={ character.caste }
+                label={ character.aspect ? 'Aspect' : 'Caste' } onChange={ handleChange } margin="dense"
+              /><br />
+              <TextField name="exalt_type" value={ character.exalt_type }
+                label="Type" onChange={ handleChange } margin="dense"
+              /><br />
+              Has Castes&nbsp;&nbsp;&nbsp;
+              <FormControlLabel
+                control={
+                  <Switch checked={ character.aspect } name="aspect" onChange={ handleAspectChange } />
+                }
+                label="Has Aspects"
+              />
+            </Typography>
+          }
+
         </DialogContent>
         <DialogActions>
           <Button onClick={ handleClose }>Cancel</Button>

424fbfbed28214656a3bd32f9f8e4adaf3e25e0b
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2018-03-29 17:07:37 -0700

    Improve input handling for attributes, abilities, essence, willpower

--- app/javascript/lca/utils/index.js
+++ app/javascript/lca/utils/index.js
@@ -1,3 +1,15 @@
 export function clamp(val, min, max) {
-  return Math.max(Math.min(val, max), min)
+  if (val > max) {
+    if (max === 10 && val === 10) // Willpower, essence can be exactly 10
+      val = 10
+    else if (max <= 10)
+      val = val % 10
+    else
+      val = max
+  }
+
+  if (val < min)
+    val = min
+
+  return val
 }

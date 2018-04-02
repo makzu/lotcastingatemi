b0fe442edfc9fa0ae7b018280db2017cb900849c
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2018-04-01 23:57:44 -0700

    Make resources page more readable on dark theme

--- app/javascript/lca/components/pages/resourcesPage.jsx
+++ app/javascript/lca/components/pages/resourcesPage.jsx
@@ -10,34 +10,28 @@ export default function ResourcesPage() {
       Resources
     </Typography>
 
-    <Typography paragraph>
-      <a href="https://www.reddit.com/r/exalted/comments/4yby39/madletters_charm_cascades_version_3_including/">
-        Charm Cascades and other resources
-      </a>
-      &nbsp;
-      by MadLetter
+    <Typography paragraph
+      component="a"  href="https://www.reddit.com/r/exalted/comments/4yby39/madletters_charm_cascades_version_3_including/"
+    >
+      Charm Cascades and other resources by MadLetter (On Reddit)
     </Typography>
 
-    <Typography paragraph>
-      <a href="http://forum.theonyxpath.com/forum/main-category/exalted/766048-ex3-i-made-a-1-page-combat-flowchart">
-        Combat flowchart
-      </a>
-      &nbsp;
-      by GivenFlesh
+    <Typography paragraph
+      component="a" href="http://forum.theonyxpath.com/forum/main-category/exalted/766048-ex3-i-made-a-1-page-combat-flowchart"
+    >
+      Combat flowchart by GivenFlesh (OPP Forums)
     </Typography>
 
-    <Typography paragraph>
-      <a href="http://forum.theonyxpath.com/forum/main-category/exalted/1180122-ex3-social-system-cheat-sheet-and-flowchart">
-        Social system cheat sheet
-      </a>
-      &nbsp;
-      by Redthorn
+    <Typography paragraph
+      component="a" href="http://forum.theonyxpath.com/forum/main-category/exalted/1180122-ex3-social-system-cheat-sheet-and-flowchart"
+    >
+      Social system cheat sheet by Redthorn (Opp Forums)
     </Typography>
 
-    <Typography paragraph>
-      <a href="http://forum.theonyxpath.com/forum/main-category/exalted/65025-exalted-3rd-edition-useful-links-and-topics">
-        &apos;Useful Links&apos; thread on the official Exalted forums
-      </a>
+    <Typography paragraph
+      component="a" href="http://forum.theonyxpath.com/forum/main-category/exalted/65025-exalted-3rd-edition-useful-links-and-topics"
+    >
+      &quot;Useful Links&quot; thread on the official Exalted forums
     </Typography>
   </BlockPaper>
 }

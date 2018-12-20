b07d59bf9e0d30cb1e954d1994dfb83ef022126b
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2018-12-19 23:19:15 -0800

    Move Redux store config to its own file

--- app/javascript/lca/index.js
+++ app/javascript/lca/index.js
@@ -4,36 +4,13 @@ require('typeface-roboto')
 import { createBrowserHistory } from 'history'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
-import { apiMiddleware } from 'redux-api-middleware'
 
-import authTokenMiddleware from './middleware/authTokenMiddleware.js'
-import themeSaverMiddleware from './middleware/themeSaverMiddleware.js'
-import navigatorMiddleware from './middleware/navigatorMiddleware.js'
-
-import reducer from './ducks'
+import configureStore from './store.js'
 import { lcaInit } from './ducks/actions.js'
 
 import RootContainer from './containers/rootContainer.jsx'
 
-export const history = createBrowserHistory()
-
-const makeEnhancer =
-  process.env.NODE_ENV === 'production' ? compose : composeWithDevTools
-
-const enhancer = makeEnhancer(
-  applyMiddleware(
-    thunk,
-    apiMiddleware,
-    authTokenMiddleware,
-    themeSaverMiddleware,
-    navigatorMiddleware
-  )
-)
-
-const store = createStore(reducer, undefined, enhancer)
+const store = configureStore()
 
 store.dispatch(lcaInit())
 

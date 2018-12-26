075566c2e8c82f59c3c17de9b34960eff02acb8e
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2018-12-26 17:11:52 -0600

    Add offline caching of entities

--- app/javascript/lca/ducks/entities/index.js
+++ app/javascript/lca/ducks/entities/index.js
@@ -116,6 +116,9 @@ export function EntityReducer(
     case LOGOUT:
     case PLY_DESTROY_SUCCESS:
       return defaultState
+    case 'persist/REHYDRATE':
+      // Replace default state with rehydrated state
+      return action.payload.entities.current
   }
 
   return state

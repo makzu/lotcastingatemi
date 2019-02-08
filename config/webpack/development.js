e96f8c14dfa18f34d956956c89516a4300075984
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2019-02-07 17:31:15 -0800

    Fix hot-reload crash

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+environment.config.merge({
+  optimization: {
+    splitChunks: {
+      name: true,
+    },
+  },
+})
+
 module.exports = environment.toWebpackConfig()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,13 @@
 // https://github.com/rails/webpacker/blob/master/docs/webpack.md
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
-const HashedChunkidsPlugin = require('webpack-hashed-chunkids')
+
 const typescript = require('./loaders/typescript')
 
 environment.plugins.prepend(
   'HashedModuleIds',
-  new webpack.HashedModuleIdsPlugin({})
+  new webpack.HashedModuleIdsPlugin()
 )
-
-environment.plugins.append('HashedChunkIds', new HashedChunkidsPlugin())
 
 environment.resolvedModules.append('vendor', 'vendor')
 

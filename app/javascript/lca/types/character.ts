a6436e4ef99744d36224baf0f929383defb19378
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2019-03-21 17:49:47 -0700

    Finish typescript definitions for characters

--- app/javascript/lca/types/character.ts
+++ app/javascript/lca/types/character.ts
@@ -1,4 +1,4 @@
-import { PlayerAsset } from './_lib'
+import { PlayerAsset, Weight } from './_lib'
 import { WithSharedStats } from './shared'
 
 export type Ability =
@@ -63,10 +63,90 @@ export interface Character extends PlayerAsset, WithSharedStats {
   favored_attributes: Attribute[]
   caste_abilities: Ability[]
   favored_abilities: Ability[]
+  supernal_ability: Ability
+  xp_log: XpLogEntry[]
+  xp_total: number
+  xp_log_solar: XpLogEntry[]
+  xp_solar_total: number
+  bp_log: XpLogEntry[]
+  equipment: string
+  notes: string
   totem: string
   tell: string
   forms: Form[]
-  sort_order: number
-  pinned: boolean
-  hidden: boolean
+  merits: number[]
+  weapons: number[]
+  attr_strength: number
+  attr_dexterity: number
+  attr_stamina: number
+  attr_charisma: number
+  attr_manipulation: number
+  attr_appearance: number
+  attr_perception: number
+  attr_intelligence: number
+  attr_wits: number
+  abil_archery: number
+  abil_athletics: number
+  abil_awareness: number
+  abil_brawl: number
+  abil_bureaucracy: number
+  abil_dodge: number
+  abil_integrity: number
+  abil_investigation: number
+  abil_larceny: number
+  abil_linguistics: number
+  abil_lore: number
+  abil_medicine: number
+  abil_melee: number
+  abil_occult: number
+  abil_performance: number
+  abil_presence: number
+  abil_resistance: number
+  abil_ride: number
+  abil_sail: number
+  abil_socialize: number
+  abil_stealth: number
+  abil_survival: number
+  abil_thrown: number
+  abil_war: number
+  abil_craft: CraftRating[]
+  abil_martial_arts: MARating[]
+  specialties: Specialty[]
+  armor_name: string
+  armor_weight: 'unarmored' | Weight
+  armor_is_artifact: boolean
+  armor_tags: string[]
+  bonus_hardness: number
+  bonus_soak: number
+  bonus_mobility_penalty: number
+  xp_craft_silver: number
+  xp_craft_gold: number
+  xp_craft_white: number
+  anima_powers: any[]
+  limit_trigger: string
+  limit: number
+
+  excellency: string
+  excellency_stunt: string
+  excellencies_for: string[]
+  charms: number[]
+  martial_arts_charms: number[]
+  evocations: number[]
+  spirit_charms: number[]
+  poisons: number[]
+}
+
+export interface CraftRating {
+  craft: string
+  rating: number
+}
+
+export interface MARating {
+  style: string
+  rating: number
+}
+
+export interface Specialty {
+  ability: Ability
+  context: string
 }
--- app/javascript/lca/types/index.ts
+++ app/javascript/lca/types/index.ts
@@ -2,39 +2,13 @@ export * from './character'
 export * from './qc'
 export * from './battlegroup'
 export * from './chronicle'
-export * from './spell'
+export * from './traits'
 
-import { Sortable } from 'utils'
-import { WithId } from './_lib'
 import { Ability, Attribute } from './character'
 
 export type CharacterType = 'character' | 'qc' | 'battlegroup'
 
-interface CharacterTrait extends WithId, Sortable {
-  character_id: number
-}
-
-export interface IMerit extends CharacterTrait {
-  merit: string
-  label: string
-  merit_name: string
-  supernatural: boolean
-  merit_cat: 'story' | 'innate' | 'purchased' | 'flaw'
-  description: string
-  ref: string
-  rating: 0 | 1 | 2 | 3 | 4 | 5 | 6
-}
-
-export interface ICharm extends CharacterTrait {
-  ability: Ability | Attribute
-}
-
-export interface IPoison extends WithId, Sortable {
-  poisonable_id: number
-  poisonable_type: CharacterType
-}
-
-export interface IPoolBase {
+export interface PoolBase {
   name: string
   ability: Ability
   attribute: Attribute

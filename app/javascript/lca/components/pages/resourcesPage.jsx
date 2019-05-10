515d713b6f83f665e23407a36e62d0a7502e85a5
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2019-05-10 15:26:02 -0700

    Migrate Resources page to Markdown

--- app/javascript/lca/components/pages/resourcesPage.jsx
+++ app/javascript/lca/components/pages/resourcesPage.jsx
@@ -1,56 +1,13 @@
 // @flow
 import React from 'react'
 
-import Typography from '@material-ui/core/Typography'
-
+import MarkdownDisplay from 'components/generic/MarkdownDisplay.jsx'
 import BlockPaper from '../generic/blockPaper.jsx'
+import ResourcesText from 'Docs'
 
 const ResourcesPage = () => (
   <BlockPaper>
-    <Typography variant="h5" gutterBottom>
-      Resources
-    </Typography>
-
-    <Typography
-      paragraph
-      component="a"
-      href="https://www.reddit.com/r/exalted/comments/4yby39/madletters_charm_cascades_version_3_including/"
-    >
-      Solar / Martial Arts Charm Cascades and other resources by MadLetter
-      (Reddit)
-    </Typography>
-
-    <Typography
-      paragraph
-      component="a"
-      href="https://www.reddit.com/r/exalted/comments/8barde/madletters_dragonblooded_cascades/"
-    >
-      Dragon-Blooded Charm Cascades by MadLetter (Reddit)
-    </Typography>
-
-    <Typography
-      paragraph
-      component="a"
-      href="http://forum.theonyxpath.com/forum/main-category/exalted/766048-ex3-i-made-a-1-page-combat-flowchart"
-    >
-      Combat flowchart by GivenFlesh (OPP Forums)
-    </Typography>
-
-    <Typography
-      paragraph
-      component="a"
-      href="http://forum.theonyxpath.com/forum/main-category/exalted/1180122-ex3-social-system-cheat-sheet-and-flowchart"
-    >
-      Social system cheat sheet by Redthorn (OPP Forums)
-    </Typography>
-
-    <Typography
-      paragraph
-      component="a"
-      href="http://forum.theonyxpath.com/forum/main-category/exalted/65025-exalted-3rd-edition-useful-links-and-topics"
-    >
-      &quot;Useful Links&quot; thread on the official Exalted forums
-    </Typography>
+    <MarkdownDisplay source={ResourcesText} />
   </BlockPaper>
 )
 export default ResourcesPage
---
+++ docs/resources.md
@@ -0,0 +1,33 @@
+# Resources
+
+### Charm Cascades
+
+The Incomparable MadLetter has created 2e-style [Charm Cascades for Solar, Dragon-Blooded, and Lunar Charms](https://www.reddit.com/r/exalted/comments/b0e9t5/madletters_charm_cascades_lunar_solar/), as well as the Martial Arts styles in the core book.
+
+### Flowcharts and Guides
+
+[Combat flowchart by GivenFlesh](http://forum.theonyxpath.com/forum/main-category/exalted/766048-ex3-i-made-a-1-page-combat-flowchart)
+
+[Social system cheat sheet by Redthorn](http://forum.theonyxpath.com/forum/main-category/exalted/1180122-ex3-social-system-cheat-sheet-and-flowchart)
+
+[Another social cheat sheet](https://i.imgur.com/AgSGjX2.png), creator unknown
+
+### Alternative Character Sheets
+
+[Interactive PDF Sheet](https://mrgone.rocksolidshells.com/index.html) by MrGone (Click on 'other sheets' in the sidebar to access)
+
+[Master Sheet](https://docs.google.com/spreadsheets/d/1pfjmZKzcUqAX9mB58IAEUIFkZr8rq4CvdRRM4kzwwgU/edit?usp=sharing) (Google Docs spreadsheet)
+
+[Advanced Character Generator](http://forum.theonyxpath.com/forum/main-category/exalted/1087290-advanced-character-generator) by Refhi (Excel/LibreOffice spreadsheet)
+
+[Familiar Generator](http://forum.theonyxpath.com/forum/main-category/exalted/1141199-exalted-3-familiar-generator) also by Refhi (Excel/LibreOffice spreadsheet)
+
+[Battlegroup stat generator](https://colin-fredericks.github.io/ex3-battle-groups/) (Online app)
+
+### Other Resources
+
+[Interactive travel times map](http://armrha.org/exaltedmap/)
+
+[Wise Artificer's Insight](http://wiseartificersinsight.com/), a Solar crafting calculator
+
+["Useful Links" thread on the official forums](http://forum.theonyxpath.com/forum/main-category/exalted/65025-exalted-3rd-edition-useful-links-and-topics)

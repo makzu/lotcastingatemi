d5bef8100d095ac9b80e309aabfad26f25815eaa
Bill Weepie <makzukun@gmail.com>
2022-05-25 21:54:22 -0700

    Fix credentials-related issues in onboarding setup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :google_oauth2, Rails.application.credentials.google_client_id, Rails.application.credentials.google_secret, verify_iss: false
+  provider :google_oauth2, Rails.application.credentials.google_client_id, Rails.application.credentials.google_secret, verify_iss: false if Rails.env.production?
 end

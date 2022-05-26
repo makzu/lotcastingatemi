ed332e72ac82541dbe48823679d135b9bea20be7
Bill Weepie <makzukun@gmail.com>
2022-05-25 21:54:46 -0700

    Update update_broadcast_job.rb
    Fixes an issue running db:seed

--- app/jobs/update_broadcast_job.rb
+++ app/jobs/update_broadcast_job.rb
@@ -6,7 +6,9 @@ class UpdateBroadcastJob < ApplicationJob
 
   def perform(ids, entity, changes)
     ids.each do |id|
-      if [entity.player.id, entity&.storyteller&.id].include? id
+      if (entity.is_a? Player) || (entity.is_a? Chronicle)
+        broadcast_update id, entity, changes
+      elsif [entity.player.id, entity&.storyteller&.id].include? id
         broadcast_update id, entity, changes
       else
         # Remove hidden intimacies from broadcasted changes

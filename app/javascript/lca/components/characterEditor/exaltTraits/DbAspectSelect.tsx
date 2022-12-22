185292a95cc376c63303d927aea554a877867088
Hush Now, Quiet Now <makzukun@gmail.com>
2022-12-21 23:14:28 -0800

    Migrate some caste/aspect selectors to Typescript

---
+++ app/javascript/lca/components/characterEditor/exaltTraits/DbAspectSelect.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+
+import ListSubheader from '@material-ui/core/ListSubheader'
+import MenuItem from '@material-ui/core/MenuItem'
+import TextField, { TextFieldProps } from '@material-ui/core/TextField'
+
+const DbAspectSelect = (props: TextFieldProps) => (
+  <TextField
+    select
+    {...props}
+    name="caste"
+    label="Aspect"
+    margin="dense"
+    data-cy="select-db-aspect"
+  >
+    <ListSubheader key="none"> Select an Aspect </ListSubheader>
+    <MenuItem value="air">Air</MenuItem>
+    <MenuItem value="earth">Earth</MenuItem>
+    <MenuItem value="fire">Fire</MenuItem>
+    <MenuItem value="water">Water</MenuItem>
+    <MenuItem value="wood">Wood</MenuItem>
+  </TextField>
+)
+
+export default DbAspectSelect
---
+++ app/javascript/lca/components/characterEditor/exaltTraits/SolarCasteSelect.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+
+import ListSubheader from '@material-ui/core/ListSubheader'
+import MenuItem from '@material-ui/core/MenuItem'
+import TextField, { TextFieldProps } from '@material-ui/core/TextField'
+
+const SolarCasteSelect = (props: TextFieldProps) => (
+  <TextField
+    select
+    {...props}
+    name="caste"
+    label="Caste"
+    margin="dense"
+    data-cy="select-solar-caste"
+  >
+    <ListSubheader key="none">Select a Caste</ListSubheader>
+    <MenuItem value="dawn">Dawn</MenuItem>
+    <MenuItem value="zenith">Zenith</MenuItem>
+    <MenuItem value="twilight">Twilight</MenuItem>
+    <MenuItem value="night">Night</MenuItem>
+    <MenuItem value="eclipse">Eclipse</MenuItem>
+  </TextField>
+)
+
+export default SolarCasteSelect

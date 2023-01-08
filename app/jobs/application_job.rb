99708e9f15d42bcac2e564972561f10fe3b44ed9
Hush Now, Quiet Now <makzukun@gmail.com>
2023-01-08 01:57:51 +0000

    Fix broken spec on Ruby 3.x for broadcast jobs

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -5,36 +5,42 @@ class ApplicationJob < ActiveJob::Base
   def broadcast_create(id, entity, json, parent_type, parent_id)
     ActionCable.server.broadcast(
       "entity-update-#{id}",
-      event:       'create',
-      type:        entity.entity_type,
-      assoc:       "#{entity.entity_assoc}s",
-      id:          entity.id,
-      entity:      json,
-      parent_type: "#{parent_type}s",
-      parent_id:   parent_id
+      {
+        event:       'create',
+        type:        entity.entity_type,
+        assoc:       "#{entity.entity_assoc}s",
+        id:          entity.id,
+        entity:      json,
+        parent_type: "#{parent_type}s",
+        parent_id:   parent_id
+      }
     )
   end
 
   def broadcast_update(id, entity, changes)
     ActionCable.server.broadcast(
       "entity-update-#{id}",
-      event:   'update',
-      type:    entity.entity_type,
-      id:      entity.id,
-      changes: changes
+      {
+        event:   'update',
+        type:    entity.entity_type,
+        id:      entity.id,
+        changes: changes
+      }
     )
   end
 
   def broadcast_destroy(id, entity, parent_type, parent_id, chronicle_id)
     ActionCable.server.broadcast(
       "entity-update-#{id}",
-      event:        'destroy',
-      type:         entity.entity_type,
-      assoc:        "#{entity.entity_assoc}s",
-      id:           entity.id,
-      parent_type:  "#{parent_type}s",
-      parent_id:    parent_id,
-      chronicle_id: chronicle_id
+      {
+        event:        'destroy',
+        type:         entity.entity_type,
+        assoc:        "#{entity.entity_assoc}s",
+        id:           entity.id,
+        parent_type:  "#{parent_type}s",
+        parent_id:    parent_id,
+        chronicle_id: chronicle_id
+      }
     )
   end
 end

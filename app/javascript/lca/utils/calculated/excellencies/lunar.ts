332b450c46ae8c653ac05275f456cd38ced7f686
Hush Now, Quiet Now <makzukun@gmail.com>
2023-01-21 06:52:00 +0000

    Fix eslint errors with Lunar Excellency

--- app/javascript/lca/utils/calculated/excellencies/lunar.ts
+++ app/javascript/lca/utils/calculated/excellencies/lunar.ts
@@ -6,13 +6,14 @@ import { highestOtherAttribute } from './custom.js'
 export const lunarExcellencyAbils = (character: Character, charms: Charm[]) => {
   const charmsPerAttribute = {}
   const casteAndFav: string[] = character.caste_attributes.concat(
-    character.favored_attributes
+    character.favored_attributes,
   )
-  charms.forEach(ch => {
-    charmsPerAttribute[ch.ability] = (charmsPerAttribute[ch.ability] || 0) + 1
+  charms.forEach((ch) => {
+    charmsPerAttribute[ch.ability || ''] =
+      (charmsPerAttribute[ch.ability || ''] || 0) + 1
   })
 
-  const attributes = ATTRIBUTES.map(a => {
+  const attributes = ATTRIBUTES.map((a) => {
     const pretty = a.pretty.toLowerCase()
 
     if (
@@ -33,12 +34,12 @@ const LunarExcellency = (
   attribute: Attribute,
   ability: Ability,
   staticRating: boolean,
-  stunt: boolean = false
+  stunt = false,
 ) =>
   Math.floor(
     (attr(character, attribute) +
       (stunt ? highestOtherAttribute(character, attribute) : 0)) /
-      (staticRating ? 2 : 1)
+      (staticRating ? 2 : 1),
   )
 
 export default LunarExcellency

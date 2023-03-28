615b988e03c259d5283c2f631c0dd068eb170886
Hush Now, Quiet Now <makzukun@gmail.com>
2023-03-27 22:33:42 -0700

    Fix compile failure due to paths

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,15 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
+      '@': resolve(__dirname, 'app/javascript/lca'),
       Docs: resolve(__dirname, 'docs'),
+      'components': resolve(__dirname, 'app/javascript/lca/components'),
+      'containers':resolve(__dirname, 'app/javascript/lca/containers'),
+      'ducks': resolve(__dirname, 'app/javascript/lca/ducks'),
+      'icons': resolve(__dirname, 'app/javascript/lca/icons'),
+      'selectors': resolve(__dirname, 'app/javascript/lca/selectors'),
+      'styles': resolve(__dirname, 'app/javascript/lca/styles'),
+      'utils': resolve(__dirname, 'app/javascript/lca/utils'),
     },
   },
 })

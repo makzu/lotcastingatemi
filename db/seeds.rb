2456ec4e7236e07a33162bab682b0e80fb9b1ab6
Hush Now, Quiet Now <makzukun@gmail.com>
2024-01-13 06:30:52 +0000

    Rubocop seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,63 +27,63 @@ pc.update(
 )
 
 puts 'Reloading characters'
-YAML.safe_load(File.read('db/seeds/characters.yaml')).each do |c|
+YAML.safe_load_file('db/seeds/characters.yaml').each do |c|
   character = Character.find_or_initialize_by(id: c['id'])
   character.update(c)
   puts "Updated #{character.name}"
 end
 
 puts 'Reloading merits'
-YAML.safe_load(File.read('db/seeds/merits.yaml')).each do |m|
+YAML.safe_load_file('db/seeds/merits.yaml').each do |m|
   merit = Merit.find_or_initialize_by(id: m['id'])
   merit.update(m)
-  puts "Updated merit #{merit.label || merit.merit_name} for #{merit.character.name}"
+  puts "Updated merit #{merit.label.presence || merit.merit_name} for #{merit.character.name}"
 end
 
 puts 'Reloading weapons'
-YAML.safe_load(File.read('db/seeds/weapons.yaml')).each do |w|
+YAML.safe_load_file('db/seeds/weapons.yaml').each do |w|
   weapon = Weapon.find_or_initialize_by(id: w['id'])
   weapon.update(w)
   puts "Updated weapon #{weapon.name} for #{weapon.character.name}"
 end
 
 puts 'Reloading Charms'
-YAML.safe_load(File.read('db/seeds/charms.yaml')).each do |c|
+YAML.safe_load_file('db/seeds/charms.yaml').each do |c|
   charm = Charm.find_or_initialize_by(id: c['id'])
   charm.update(c)
   puts "Updated #{charm.charm_type} Charm #{charm.name} for #{charm.character.name}"
 end
 
 puts 'Reloading QCs'
-YAML.safe_load(File.read('db/seeds/qcs.yaml')).each do |q|
+YAML.safe_load_file('db/seeds/qcs.yaml').each do |q|
   qc = Qc.find_or_initialize_by(id: q['id'])
   qc.update(q)
   puts "Updated QC #{qc.name}"
 end
 
 puts 'Reloading QC merits'
-YAML.safe_load(File.read('db/seeds/qc_merits.yaml')).each do |m|
+YAML.safe_load_file('db/seeds/qc_merits.yaml').each do |m|
   merit = QcMerit.find_or_initialize_by(id: m['id'])
   merit.update(m)
   puts "Updated QC Merit #{merit.name} for #{merit.qc.name}"
 end
 
 puts 'Reloading QC Charms'
-YAML.safe_load(File.read('db/seeds/qc_charms.yaml')).each do |c|
+YAML.safe_load_file('db/seeds/qc_charms.yaml').each do |c|
   charm = QcCharm.find_or_initialize_by(id: c['id'])
   charm.update(c)
   puts "Updated QC Charm #{charm.name} for #{charm.qc.name}"
 end
 
 puts 'Reloading Battlegroups'
-YAML.safe_load(File.read('db/seeds/battlegroups.yaml')).each do |a|
+YAML.safe_load_file('db/seeds/battlegroups.yaml').each do |a|
   battlegroup = Battlegroup.find_or_initialize_by(id: a['id'])
   battlegroup.update(a)
   puts "Updated Battlegroup #{battlegroup.name}"
 end
 
 puts 'Reloading QC attacks'
-YAML.safe_load(File.read('db/seeds/qc_attacks.yaml')).each do |a|
+YAML.safe_load_file('db/seeds/qc_attacks.yaml').each do |a|
   attack = QcAttack.find_or_initialize_by(id: a['id'])
   attack.update(a)
   puts "Updated QC Attack #{attack.name} for #{attack.qc_attackable.name}"

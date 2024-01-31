349a7c606f4e3b6fe390e0c0e9dc26093c3c035c
Hush Now, Quiet Now <makzukun@gmail.com>
2024-01-31 02:15:49 +0000

    PlayerName Subtitle: connect -> Hooks

--- app/javascript/lca/components/generic/PlayerNameSubtitle.tsx
+++ app/javascript/lca/components/generic/PlayerNameSubtitle.tsx
@@ -1,33 +1,21 @@
-import { connect } from 'react-redux'
-
-import { getSpecificPlayer } from '@/selectors'
-
-import type { RootState } from 'store'
 import { Typography } from '@mui/material'
 
-interface ExposedProps {
-  playerId: number
-}
-type Props = ExposedProps & {
-  name: string
-}
+import { getSpecificPlayer } from '@/selectors'
+import { useAppSelector } from '@/hooks'
 
-function PlayerNameSubtitle({ name }: Props) {
+function PlayerNameSubtitle({ playerId }: { playerId: number }) {
+  const name = useAppSelector(
+    (state) => getSpecificPlayer(state, playerId)?.display_name,
+  )
   return (
     <Typography
       variant="caption"
       component="div"
-      style={{
-        marginTop: '-0.25rem',
-      }}
+      sx={{ marginTop: '-0.25rem' }}
     >
       Player: {name}
     </Typography>
   )
 }
 
-const mapStateToProps = (state: RootState, props: ExposedProps) => ({
-  name: getSpecificPlayer(state, props.playerId).display_name,
-})
-
-export default connect(mapStateToProps)(PlayerNameSubtitle)
+export default PlayerNameSubtitle

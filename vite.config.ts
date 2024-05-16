99de72ebc0eef67e64e629c16b174e1f9e617691
Hush Now, Quiet Now <makzukun@gmail.com>
2024-05-16 20:07:36 +0000

    Update Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,12 @@
 import React from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { defineConfig } from 'vite'
 import Rails from 'vite-plugin-rails'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   assetsInclude: ['**/*.md'],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            // if (id.includes('@mui')) {
-            //   return 'vendor-mui'
-            // }
-            // if (id.includes('react-markdown')) {
-            //   return 'vendor-react-markdown'
-            // }
-            if (
-              id.includes('react-markdown') ||
-              id.includes('micromark') ||
-              id.includes('property-information')
-            ) {
-              return 'vendor-react-markdown'
-            }
-            if (id.includes('react-router') || id.includes('@remix-run')) {
-              return 'vendor-react-router'
-            }
-            if (
-              id.includes('node_modules/react/') ||
-              id.includes('react-dom')
-            ) {
-              return 'vendor-react'
-            }
-            // return 'vendor'
-          }
-        },
-      },
-    },
-  },
 
   plugins: [
     Rails({
@@ -49,7 +16,6 @@ export default defineConfig({
       },
     }),
     React(),
-    splitVendorChunkPlugin(),
     svgr(),
     tsconfigPaths(),
   ],
@@ -71,7 +37,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['testSetup.ts'],
     exclude: ['**/__test__/**'],
-    includeSource: ['app/frontend/**/*.ts'],
+    includeSource: ['app/javascript/lca/**/*.(ts|tsx)'],
   },
   define: {
     'import.meta.vitest': undefined,

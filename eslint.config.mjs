0e5979d7184b3668369efe1c54322d17802e8171
Hush Now, Quiet Now <makzukun@gmail.com>
2026-04-19 21:14:30 +0000

    Migrate to Biome because eslint was annoying

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,3 @@
-// @ts-check
+import biome from 'eslint-config-biome'
 
-import eslint from '@eslint/js'
-import prettier from 'eslint-config-prettier'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  react.configs.flat.recommended,
-  reactHooks.configs['recommended-latest'],
-  reactRefresh.configs.vite,
-  prettier,
-  {
-    files: ['*.js', '*.jsx'],
-    rules: {
-      '@typescript-eslint/ban-types': 'off',
-    },
-  },
-)
+export default [biome]

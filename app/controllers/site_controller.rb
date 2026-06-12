34d13b1e6767f3833024dcb3596b1a7a1071bf17
Hush Now, Quiet Now <makzukun@gmail.com>
2026-06-12 00:10:45 +0000

    Fix brakeman warning in site_controller.rb

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,7 +4,15 @@ class SiteController < ApplicationController
   def index
     return unless %w[characters qcs battlegroups].include? params[:char]
 
-    klass = params.expect(:char).classify.constantize
+    klass = case params.expect(:char)
+            when 'characters'
+              Character
+            when 'qcs'
+              Qc
+            when 'battlegroups'
+              Battlegroup
+            end
+
     @title, @description, @image =
       klass
       .where(id: params[:id], public: true)
